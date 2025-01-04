76654da9ae1054b29ff146c73577a8c48a345a69
wheelsbot7 <o.wertzberger@google.com>
2025-01-04 15:54:16 -0500

    fixed background toggling

--- astro.config.ts
+++ astro.config.ts
@@ -9,58 +9,62 @@ import node from "@astrojs/node";
 import icon from "astro-icon";
 import rehypeCallouts from "rehype-callouts";
 
+import preact from "@astrojs/preact";
+
 const calloutOptions = {
-  theme: "github",
+	theme: "github",
 };
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://wheelsbot.dev",
-  output: "static",
-  markdown: {
-    remarkPlugins: [],
-    rehypePlugins: [[rehypeCallouts, calloutOptions]],
-  },
-  integrations: [
-    expressiveCode(),
-    mdx(),
-    sitemap(),
-    spectre({
-      name: "Wheelsblog V7",
-      themeColor: "#1d754a",
-      openGraph: {
-        home: {
-          title: "Wheelsblog V7",
-          description: "The personal site of Owen Wertzberger",
-        },
-        blog: {
-          title: "Blog",
-          description: "Guides, tutorials, and infodumps. Usually all at once.",
-        },
-        projects: {
-          title: "Stuff that I have worked on",
-        },
-      },
-      giscus: {
-        repository: "louisescher/spectre",
-        repositoryId: "R_kgDONjm3ig",
-        category: "General",
-        categoryId: "DIC_kwDONjm3is4ClmBF",
-        mapping: "pathname",
-        strict: true,
-        reactionsEnabled: true,
-        emitMetadata: false,
-        lang: "en",
-      },
-    }),
-    icon({
-      include: {
-        mdi: ["*"],
-        ri: ["*"],
-      },
-    }),
-  ],
-  adapter: node({
-    mode: "standalone",
-  }),
+	site: "https://wheelsbot.dev",
+	output: "static",
+	markdown: {
+		remarkPlugins: [],
+		rehypePlugins: [[rehypeCallouts, calloutOptions]],
+	},
+	integrations: [
+		expressiveCode(),
+		mdx(),
+		sitemap(),
+		spectre({
+			name: "Wheelsblog V7",
+			themeColor: "#1d754a",
+			openGraph: {
+				home: {
+					title: "Wheelsblog V7",
+					description: "The personal site of Owen Wertzberger",
+				},
+				blog: {
+					title: "Blog",
+					description: "Guides, tutorials, and infodumps. Usually all at once.",
+				},
+				projects: {
+					title: "Stuff that I have worked on",
+				},
+			},
+			giscus: {
+				repository: "louisescher/spectre",
+				repositoryId: "R_kgDONjm3ig",
+				category: "General",
+				categoryId: "DIC_kwDONjm3is4ClmBF",
+				mapping: "pathname",
+				strict: true,
+				reactionsEnabled: true,
+				emitMetadata: false,
+				lang: "en",
+			},
+		}),
+		icon({
+			include: {
+				mdi: ["*"],
+				ri: ["*"],
+			},
+		}),
+		preact(),
+	],
+	adapter: node({
+		mode: "standalone",
+	}),
 });
+
