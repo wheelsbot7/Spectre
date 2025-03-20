400d6119f6c2d789c770d9067715d1cab8de91f8
wheelsbot7 <o.wertzberger@gmail.com>
2025-03-19 22:31:09 -0400

    fixed search function with different keybind

--- src/pages/pagefind/pagefind.js.ts
+++ src/pages/pagefind/pagefind.js.ts
@@ -1,3 +1,7 @@
-export async function GET() {
-  return new Response('export const search = () => { return { results: [] } }');
+export async function GET() {
+  return new Response('export const search = () => { return { results: [] } }', {
+    headers: {
+      'content-type': 'application/javascript'
+    }
+  });
 }
