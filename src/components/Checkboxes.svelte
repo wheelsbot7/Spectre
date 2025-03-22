3d0b3556df729012ae39129425d50c19ecfeb271
wheelsbot7 <o.wertzberger@gmail.com>
2025-03-21 21:43:07 -0400

    EXTERMANIZED EXTERNALIZED DATA

--- src/components/Checkboxes.svelte
+++ src/components/Checkboxes.svelte
@@ -2,133 +2,7 @@
   let distro = $state("apt");
   let packages = $state([""]);
 
-  let data = [
-    {
-      Utilities: {
-        "Owncloud Client": {
-          icon: "owncloud.svg",
-          program: "owncloud-client",
-        },
-        "OpenJDK JRE": {
-          icon: "openjsk.svg",
-          program: "jre-openjdk",
-        },
-      },
-    },
-    {
-      Editors: {
-        Vim: {
-          icon: "vim.png",
-          program: "vim",
-        },
-        Neovim: {
-          icon: "neovim.png",
-          program: "neovim",
-        },
-        Eclipse: {
-          icon: "eclipseide.svg",
-          program: "eclipse-java-bin",
-        },
-        "Visual Studio Code - OSS": {
-          icon: "visualstudiocode.svg",
-          program: "code",
-        },
-        "Zed Editor": {
-          icon: "zedindustries.svg",
-          program: "zed",
-        },
-        QTCreator: {
-          icon: "qt.svg",
-          program: "qtcreator",
-        },
-      },
-    },
-    {
-      "Web Browsers": {
-        Chromium: {
-          icon: "googlechrome.svg",
-          program: "chromium",
-        },
-        Firefox: {
-          icon: "firefox.svg",
-          program: "firefox",
-        },
-        LibreWolf: {
-          icon: "firefox.svg",
-          program: "librewolf-bin",
-        },
-      },
-    },
-    {
-      "Developer Tools": {
-        "Python 3 Development": {
-          icon: "python.svg",
-          program: "python3",
-        },
-        OpenJDK: {
-          icon: "openjdk.svg",
-          program: "jdk-openjdk",
-        },
-        "OpenJDK Java 8 JDK": {
-          program: "jdk8-openjdk",
-        },
-        "gcc and g++": {
-          icon: "gnu.svg",
-          program: "gcc",
-        },
-        make: {
-          icon: "make.svg",
-          program: "make",
-        },
-      },
-    },
-    {
-      Graphics: {
-        Krita: {
-          icon: "krita.svg",
-          program: "krita",
-        },
-      },
-    },
-    {
-      Office: {
-        LibreOffice: {
-          icon: "libreoffice.svg",
-          program: "libreoffice",
-        },
-      },
-    },
-    {
-      Media: {
-        Kodi: {
-          icon: "kodi.svg",
-          program: "kodi",
-        },
-      },
-    },
-    {
-      Messaging: {},
-    },
-    {
-      Gaming: {},
-    },
-    {
-      Other: {
-        Lutris: {
-          icon: "lutris.svg",
-          program: "lutris",
-        },
-        Virtualbox: {
-          icon: "virtualbox.svg",
-          program: "virtualbox",
-        },
-        Wine: {
-          icon: "wine.svg",
-          program: "wine",
-        },
-      },
-    },
-  ];
+  import data from "/data/arch.json";
 
   let categories = data.map((item) => {
     let categoryName = Object.keys(item)[0];
@@ -183,13 +57,11 @@
   {#if packages.length === 0}
     <p>Please select at least one package</p>
   {:else}
-    <div>
-      {distro === "apt"
-        ? "sudo apt install "
-        : distro === "dnf"
-          ? "sudo dnf install"
-          : "sudo pacman -S"}
-      {join(packages)}
-    </div>
+    {distro === "apt"
+      ? "sudo apt install "
+      : distro === "dnf"
+        ? "sudo dnf install"
+        : "sudo pacman -S"}
+    {join(packages)}
   {/if}
 </div>
