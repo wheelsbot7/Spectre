326685c6180bf33e6f3275b8ab3a079f76715dbf
wheelsbot7 <o.wertzberger@gmail.com>
2025-03-22 16:09:43 -0400

    looks mostly how I want it to

--- src/components/Checkboxes.svelte
+++ src/components/Checkboxes.svelte
@@ -26,6 +26,8 @@
   import { copy } from "svelte-copy";
 </script>
 
+<h1>Linite Reloaded</h1>
+
 <h2>Distro</h2>
 
 <label>
@@ -44,61 +46,101 @@
 </label>
 
 <h2>Packages</h2>
-
-{#each categories as category}
-  {#if Object.keys(category.programs).length > 0}
-    <div class="category">{category.categoryName}</div>
-    <ul>
-      {#each Object.entries(category.programs) as [name, details]}
-        <li>
-          <label>
-            <input
-              type="checkbox"
-              bind:group={packages}
-              value={details.program}
-            />{#if details.icon}
-              <img
-                class="glow-icon"
-                src="/soft-icons/{details.icon}"
-                alt={name}
-                width="24"
-                height="24"
-                style="filter:
+<div class="container">
+  {#each categories as category}
+    {#if Object.keys(category.programs).length > 0}
+      <div class="category">
+        <h3>
+          {category.categoryName}
+        </h3>
+        <ul>
+          {#each Object.entries(category.programs) as [name, details]}
+            <li>
+              <label>
+                <input
+                  type="checkbox"
+                  bind:group={packages}
+                  value={details.program}
+                />{#if details.icon}
+                  <img
+                    loading="lazy"
+                    class="glow-icon"
+                    src="/soft-icons/{details.icon}"
+                    alt={name}
+                    width="30"
+                    height="30"
+                    style="filter:
                 invert(42%)
                 sepia(33%)
                 saturate(6029%)
                 hue-rotate(238deg)
                 brightness(99%)
                 contrast(94%)"
-              />
-            {/if}
-            {name}
-          </label>
-        </li>
-      {/each}
-    </ul>
-  {/if}
-{/each}
+                  />
+                {/if}
+                {name}
+              </label>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    {/if}
+  {/each}
+</div>
 <div>
-  {#if packages.length === 0}
-    <p>Please select at least one package</p>
-  {:else}
-    {distro === "apt"
-      ? "sudo apt install "
-      : distro === "dnf"
-        ? "sudo dnf install"
-        : "sudo pacman -S"}
-    {packages.join(" ")}
-  {/if}
+  <div class="command">
+    {#if packages.length === 0}
+      <p>Please select at least one package</p>
+    {:else}
+      {distro === "apt"
+        ? "sudo apt install "
+        : distro === "dnf"
+          ? "sudo dnf install"
+          : "sudo pacman -S"}
+      {packages.join(" ")}
+    {/if}
+  </div>
   <br />
   <button use:copy={join(packages)}> copy to clipboard </button>
 </div>
 
 <style>
+  .command {
+    display: flex;
+    font-family: monospace;
+    font-size: large;
+    border-style: outset;
+    border-color: white;
+    border-radius: 1px;
+    padding: 5px;
+  }
   button:hover {
     cursor: pointer;
   }
   button {
     border: revert;
+    margin: 10px;
+    margin-left: 0;
+  }
+  .category {
+    align-items: self;
+    display: block;
+  }
+  .container {
+    align-items: start;
+    display: block;
+  }
+  ul {
+    list-style-type: none;
+    margin-top: auto;
+  }
+  h3 {
+    margin: auto;
+  }
+  img {
+    margin-bottom: -9px;
+    margin-top: 9px;
+    margin-right: 5px;
+    margin-left: 5px;
   }
 </style>
