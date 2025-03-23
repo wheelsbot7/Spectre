b15e1ab38f9bc510c8e00e56253e959e0bdea281
wheelsbot7 <o.wertzberger@gmail.com>
2025-03-23 08:47:03 -0400

    finished debian and fedora, added project page

--- src/components/Checkboxes.svelte
+++ src/components/Checkboxes.svelte
@@ -3,22 +3,37 @@
   let packages = $state([""]);
 
   import data from "/data/arch.json";
+  import debianData from "/data/debian.json";
+  import fedoraData from "/data/fedora.json";
 
   let categories = data.map((item) => {
     let categoryName = Object.keys(item)[0];
     let programs = item[categoryName];
     return { categoryName, programs };
   });
-  function join(packages) {
-    if (packages.length === 1) return packages[0];
 
+  let debianCategories = debianData.map((item) => {
+    let categoryName = Object.keys(item)[0];
+    let programs = item[categoryName];
+    return { categoryName, programs };
+  });
+
+  let fedoraCategories = fedoraData.map((item) => {
+    let categoryName = Object.keys(item)[0];
+    let programs = item[categoryName];
+    return { categoryName, programs };
+  });
+  function join(packages) {
     switch (distro) {
       case "apt":
-        return "sudo apt install " + `${packages.join(" ")}`;
+        return (
+          "sudo apt-get update && sudo apt-get install " +
+          `${packages.join(" ")}`
+        );
       case "dnf":
         return "sudo dnf install " + `${packages.join(" ")}`;
       case "pacman":
-        return "sudo pacman -S " + `${packages.join(" ")}`;
+        return "sudo pacman -Sy " + `${packages.join(" ")}`;
       default:
         return `${packages.join(" ")}`;
     }
@@ -46,57 +61,147 @@
 </label>
 
 <h2>Packages</h2>
-<div class="container">
-  {#each categories as category}
-    {#if Object.keys(category.programs).length > 0}
-      <div class="category">
-        <h3>
-          {category.categoryName}
-        </h3>
-        <ul>
-          {#each Object.entries(category.programs) as [name, details]}
-            <li>
-              <label>
-                <input
-                  type="checkbox"
-                  bind:group={packages}
-                  value={details.program}
-                />{#if details.icon}
-                  <img
-                    loading="lazy"
-                    class="glow-icon"
-                    src="/soft-icons/{details.icon}"
-                    alt={name}
-                    width="30"
-                    height="30"
-                    style="filter:
+{#if distro == "pacman"}
+  <div class="container">
+    {#each categories as category}
+      {#if Object.keys(category.programs).length > 0}
+        <div class="category">
+          <h3>
+            {category.categoryName}
+          </h3>
+          <ul>
+            {#each Object.entries(category.programs) as [name, details]}
+              <li>
+                <label>
+                  <input
+                    type="checkbox"
+                    bind:group={packages}
+                    value={details.program}
+                  />{#if details.icon}
+                    <img
+                      loading="lazy"
+                      class="glow-icon"
+                      src="/soft-icons/{details.icon}"
+                      alt={name}
+                      width="30"
+                      height="30"
+                      style="filter:
                 invert(42%)
                 sepia(33%)
                 saturate(6029%)
                 hue-rotate(238deg)
                 brightness(99%)
                 contrast(94%)"
-                  />
-                {/if}
-                {name}
-              </label>
-            </li>
-          {/each}
-        </ul>
-      </div>
-    {/if}
-  {/each}
-</div>
+                    />
+                  {/if}
+                  {name}
+                </label>
+              </li>
+            {/each}
+          </ul>
+        </div>
+      {/if}
+    {/each}
+  </div>
+{/if}
+
+{#if distro == "apt"}
+  <div class="container">
+    {#each debianCategories as category}
+      {#if Object.keys(category.programs).length > 0}
+        <div class="category">
+          <h3>
+            {category.categoryName}
+          </h3>
+          <ul>
+            {#each Object.entries(category.programs) as [name, details]}
+              <li>
+                <label>
+                  <input
+                    type="checkbox"
+                    bind:group={packages}
+                    value={details.program}
+                  />{#if details.icon}
+                    <img
+                      loading="lazy"
+                      class="glow-icon"
+                      src="/soft-icons/{details.icon}"
+                      alt={name}
+                      width="30"
+                      height="30"
+                      style="filter:
+                invert(42%)
+                sepia(33%)
+                saturate(6029%)
+                hue-rotate(238deg)
+                brightness(99%)
+                contrast(94%)"
+                    />
+                  {/if}
+                  {name}
+                </label>
+              </li>
+            {/each}
+          </ul>
+        </div>
+      {/if}
+    {/each}
+  </div>
+{/if}
+
+{#if distro == "dnf"}
+  <div class="container">
+    {#each fedoraCategories as category}
+      {#if Object.keys(category.programs).length > 0}
+        <div class="category">
+          <h3>
+            {category.categoryName}
+          </h3>
+          <ul>
+            {#each Object.entries(category.programs) as [name, details]}
+              <li>
+                <label>
+                  <input
+                    type="checkbox"
+                    bind:group={packages}
+                    value={details.program}
+                  />{#if details.icon}
+                    <img
+                      loading="lazy"
+                      class="glow-icon"
+                      src="/soft-icons/{details.icon}"
+                      alt={name}
+                      width="30"
+                      height="30"
+                      style="filter:
+                invert(42%)
+                sepia(33%)
+                saturate(6029%)
+                hue-rotate(238deg)
+                brightness(99%)
+                contrast(94%)"
+                    />
+                  {/if}
+                  {name}
+                </label>
+              </li>
+            {/each}
+          </ul>
+        </div>
+      {/if}
+    {/each}
+  </div>
+{/if}
 <div>
   <div class="command">
     {#if packages.length === 0}
       <p>Please select at least one package</p>
     {:else}
       {distro === "apt"
-        ? "sudo apt install "
+        ? "sudo apt-get update && sudo apt-get install "
         : distro === "dnf"
           ? "sudo dnf install"
-          : "sudo pacman -S"}
+          : "sudo pacman -Sy"}
       {packages.join(" ")}
     {/if}
   </div>
