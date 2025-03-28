74eb8cbe1069f94e8049598810b627325cc21af4
wheelsbot7 <o.wertzberger@gmail.com>
2025-03-28 12:08:35 -0400

    Squash merge master into drafting

--- astro.config.ts
+++ astro.config.ts
@@ -12,6 +12,18 @@ import vercel from "@astrojs/vercel";
 import icon from "astro-icon";
 import rehypeCallouts from "rehype-callouts";
 
+import svelte from "@astrojs/svelte";
+
+import react from "@astrojs/react";
+
+import preact from "@astrojs/preact";
+
+import solidJs from "@astrojs/solid-js";
+
+import vue from "@astrojs/vue";
+
+import alpinejs from "@astrojs/alpinejs";
+
 const calloutOptions = {
   theme: "github",
 };
@@ -24,97 +36,50 @@ export default defineConfig({
     remarkPlugins: [],
     rehypePlugins: [[rehypeCallouts, calloutOptions]],
   },
-  integrations: [
-    pagefind(),
-    expressiveCode({
-      plugins: [pluginLineNumbers()],
-    }),
-    mdx(),
-    sitemap(),
-    spectre({
-      name: "Wheelsblog V7",
-      themeColor: "#1d754a",
-      openGraph: {
-        home: {
-          title: "Wheelsblog V7",
-          description: "The personal site of Owen Wertzberger",
-        },
-        blog: {
-          title: "Blog",
-          description: "Guides, tutorials, and infodumps. Usually all at once.",
-        },
-        projects: {
-          title: "Stuff that I have worked on",
-        },
+  integrations: [pagefind(), expressiveCode({
+    plugins: [pluginLineNumbers()],
+  }), mdx(), sitemap(), spectre({
+    name: "Wheelsblog V7",
+    themeColor: "#1d754a",
+    openGraph: {
+      home: {
+        title: "Wheelsblog V7",
+        description: "The personal site of Owen Wertzberger",
       },
-      giscus: {
-        repository: "wheelsbot7/spectre",
-        repositoryId: "R_kgDONkaxYQ",
-        category: "Announcements",
-        categoryId: "DIC_kwDONkaxYc4Cl0ul",
-        mapping: "title",
-        strict: true,
-        reactionsEnabled: true,
-        emitMetadata: false,
-        lang: "en",
+      blog: {
+        title: "Blog",
+        description: "Guides, tutorials, and infodumps. Usually all at once.",
       },
-    }),
-    icon({
-      include: {
-        mdi: ["*"],
-        ri: ["*"],
+      projects: {
+        title: "Stuff that I have worked on",
       },
-    }),
-  ],
+    },
+    giscus: {
+      repository: "wheelsbot7/spectre",
+      repositoryId: "R_kgDONkaxYQ",
+      category: "Announcements",
+      categoryId: "DIC_kwDONkaxYc4Cl0ul",
+      mapping: "title",
+      strict: true,
+      reactionsEnabled: true,
+      emitMetadata: false,
+      lang: "en",
+    },
+  }), icon({
+    include: {
+      mdi: ["*"],
+      ri: ["*"],
+    },
+  }), svelte({
+    include: ["src/components/**.svelte"]
+  }), preact({
+    include: ['src/components/preact/**.jsx'],
+  }), react({
+    include: ['src/components/react/**.jsx'],
+  }), solidJs({
+    include: ['src/components/solid/**.jsx'],
+  }), vue({
+    include: ["src/components/**.vue"]
+  }), alpinejs()],
   adapter: vercel({}),
-=======
-	site: "https://wheelsbot.dev",
-	output: "static",
-	markdown: {
-		remarkPlugins: [],
-		rehypePlugins: [[rehypeCallouts, calloutOptions]],
-	},
-	integrations: [
-		expressiveCode({
-			plugins: [pluginLineNumbers()],
-		}),
-		mdx(),
-		sitemap(),
-		spectre({
-			name: "Wheelsblog V7",
-			themeColor: "#1d754a",
-			openGraph: {
-				home: {
-					title: "Wheelsblog V7",
-					description: "The personal site of Owen Wertzberger",
-				},
-				blog: {
-					title: "Blog",
-					description: "Guides, tutorials, and infodumps. Usually all at once.",
-				},
-				projects: {
-					title: "Stuff that I have worked on",
-				},
-			},
-			giscus: {
-				repository: "wheelsbot7/spectre",
-				repositoryId: "R_kgDONkaxYQ",
-				category: "Announcements",
-				categoryId: "DIC_kwDONkaxYc4Cl0ul",
-				mapping: "title",
-				strict: true,
-				reactionsEnabled: true,
-				emitMetadata: false,
-				lang: "en",
-			},
-		}),
-		icon({
-			include: {
-				mdi: ["*"],
-				ri: ["*"],
-			},
-		}),
-	],
-	adapter: vercel({}),
->>>>>>> master
 });
